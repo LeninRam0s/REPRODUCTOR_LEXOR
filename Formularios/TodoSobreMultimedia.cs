f0e255495bb5afcf3a4d40c89ef2252d5adce027
LeninRam0s <djalexander.rmix@gmail.com>
2021-04-19 18:19:04 -0600

    Actualizacion de Acciones

--- Formularios/TodoSobreMultimedia.cs
+++ Formularios/TodoSobreMultimedia.cs
@@ -27,48 +27,47 @@ namespace REPRODUCTOR_LEXOR.Formularios
             }
             return lista;
         }
+        
         public void insertarCancion(string archivo)
         {
             CapaBD datos = new CapaBD();
             datos.agregar(archivo);
-        }
+        }//INSERTAR PISTA
 
         public bool exiteCancion(string nombre)
         {
             CapaBD datos = new CapaBD();
             return datos.existeDato(nombre);
-        }
+        }//VALIDA SI EXISTE LA CANCION
 
         public void guardarLista(string nombre)
         {
             CapaBD datos = new CapaBD();
             datos.guardarLista(nombre);
-        }
+        }//GUARDA LA LISTA DE REPRODUCCION
 
         public List<string> todasLasListas()
         {
             CapaBD datos = new CapaBD();
             return datos.listasReproduccion();
-        }
+        }//RETORNA LISTAS DE REPRODUCCION
 
         public List<string> todasLasPistas()
         {
             CapaBD datos = new CapaBD();
             return datos.listaMultimedia();
-        }
+        }//RETORNA PISTAS DE LA DB
 
-
-
-        public void insertarPlaylist(string nombreLista, string nombreCancion)
+        public void insertarAPlaylist(string nombreLista, string nombreCancion)
         {
             CapaBD datos = new CapaBD();
             datos.guardarPlayList(nombreCancion, nombreLista);
-        }
+        }//INSERTA ITEMS A LAS PLAYLIST
 
         public ListaCircular reproducirPlaylist(string playlist)
         {
             CapaBD datos = new CapaBD();
             return datos.reproducirListaCircular(playlist);
-        }
+        }//REPRODUCE LA PLAYLIST SELECCIONADA
     }
 }
